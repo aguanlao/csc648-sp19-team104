359782bf38a1089646b89368c59971901ca28f67
Zachary Prince <princeguy43@gmail.com>
2019-02-15 03:47:33 +0000

    Housekeeping: Removed default SQLite DB

--- django_sites/pegasus/scripts/db-test.py
+++ django_sites/pegasus/scripts/db-test.py
@@ -1,9 +1,9 @@
 import psycopg2
 
-hostname = "localhost"
-username = "admin"
-password = "justD0it!"
-db = "pegasus-dev"
+hostname = ""
+username = ""
+password = ""
+db = ""
 conn_string = "dbname='%s' user='%s' host='%s' password='%s'" % (db, username, hostname, password)
 
 conn = psycopg2.connect(conn_string)
