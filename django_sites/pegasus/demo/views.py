61fe60e2816d434c0da3451c8f6d04afe797a712
brenna-tirumal <brennag@mail.sfsu.edu>
2019-04-27 09:52:36 -0700

    Maps file added, one pin displayed, geocoding enabled

--- django_sites/pegasus/demo/views.py
+++ django_sites/pegasus/demo/views.py
@@ -13,6 +13,7 @@ from . import utils
 from .forms import *
 from .models import *
 from pprint import pprint
+import requests
 
 #TEST
 def test(request):
@@ -471,3 +472,42 @@ def delete_group(request, group_name=None):
 def view_group(request, group_name=None):
     context = {}
     return render(request, 'demo/view_group.html', {'context': context})
+
+
+def maps(request):
+    listings = ValidListing.objects.all()
+    for listing in listings:
+        print(listing.residence.address)
+
+    # Holds geocoding data for all addresses (each of which is a dictionary)
+    all_lat_lng = []
+    for listing in listings:
+
+        geodata = dict()
+        geodata['lat'] = 0
+        geodata['lng'] = 0
+        addr = listing.residence.address
+
+        if addr:
+            GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address='+addr+'&key=AIzaSyCbr6KeU9un_uLPpH581LUfOb8PE3zi1x0'
+
+            params = {'address': addr}
+
+            map_request = requests.get(GOOGLE_MAPS_API_URL, params=params)
+            response = map_request.json()
+            #print('response: ', response)
+
+            if len(response['results']) > 0:
+                result = response['results'][0]
+                geodata['lat'] = result['geometry']['location']['lat']
+                geodata['lng'] = result['geometry']['location']['lng']
+                all_lat_lng.append(geodata)
+
+    context = {
+        'addresses': listings, 
+        'latitude': geodata['lat'],
+        'longitude': geodata['lng'],
+        'all_lat_lng': all_lat_lng,
+    }
+
+    return render(request, 'demo/maps.html', context)
